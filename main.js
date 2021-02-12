a9f5d588fb99221c702f91850a83435b3360e5f1
vladimir <mojmejlmojmejl@gmail.com>
2021-02-12 18:24:30 +0100

    added intersection oserver

--- main.js
+++ main.js
@@ -16,5 +16,54 @@ $(document).ready(function () {
         $(".tns-controls button:nth-child(2)").html('<i class="fa fa-chevron-left" aria-hidden="true"></i>');
         // ============
 
+        // scrolling animations start
+
+        const header = document.querySelector("header");
+        const sectionOne = document.querySelector(".fading-container");
+
+        const faders = document.querySelectorAll(".fade-in");
+        const sliders = document.querySelectorAll(".slide-in");
+
+        const sectionOneOptions = {
+            rootMargin: "-200px 0px 0px 0px",
+        };
+
+        const sectionOneObserver = new IntersectionObserver(function (entries, sectionOneObserver) {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    header.classList.add("nav-scrolled");
+                } else {
+                    header.classList.remove("nav-scrolled");
+                }
+            });
+        }, sectionOneOptions);
+
+        sectionOneObserver.observe(sectionOne);
+
+        const appearOptions = {
+            threshold: 0,
+            rootMargin: "0px 0px -250px 0px",
+        };
+
+        const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    return;
+                } else {
+                    entry.target.classList.add("appear");
+                    appearOnScroll.unobserve(entry.target);
+                }
+            });
+        }, appearOptions);
+
+        faders.forEach((fader) => {
+            appearOnScroll.observe(fader);
+        });
+
+        sliders.forEach((slider) => {
+            appearOnScroll.observe(slider);
+        });
+
+        // scrolling animations end
     })(jQuery);
 });
